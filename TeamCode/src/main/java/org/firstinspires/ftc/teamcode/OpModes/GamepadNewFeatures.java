0f5da515a3751a6bde8a664f4bc2a101116daecc
Zhz2025 <Zhanghz270140@163.com>
2025-07-30 19:50:27 +0800

    zhz 7-30 refactor && 从上赛季搬运一些能用的文件

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/GamepadNewFeatures.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class GamepadNewFeatures extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("test gamepad1 press and release features(所有按键均有此功能，这里只测试肩键和ABXY）", "Press start to begin");
+        telemetry.update();
+        waitForStart();
+        while( opModeIsActive() ) {
+            telemetry.addData("Gamepad 1 Left Bumper Pressed", gamepad1.leftBumperWasPressed());
+            telemetry.addData("Gamepad 1 Left Bumper Released", gamepad1.leftBumperWasReleased());
+            telemetry.addData("Gamepad 1 Left Bumper Status", gamepad1.left_bumper);
+
+            // Add an empty line to seperate the buttons in telemetry
+            telemetry.addLine();
+
+            // Add the status of the Gamepad 1 Right Bumper
+            telemetry.addData("Gamepad 1 Right Bumper Pressed", gamepad1.rightBumperWasPressed());
+            telemetry.addData("Gamepad 1 Right Bumper Released", gamepad1.rightBumperWasReleased());
+            telemetry.addData("Gamepad 1 Right Bumper Status", gamepad1.right_bumper);
+            // 添加Gamepad 1 A、B、X、Y按键的按下和释放状态
+            telemetry.addData("Gamepad 1 A Pressed", gamepad1.aWasPressed());
+            telemetry.addData("Gamepad 1 A Released", gamepad1.aWasReleased());
+            telemetry.addData("Gamepad 1 A Status", gamepad1.a);
+
+            telemetry.addData("Gamepad 1 B Pressed", gamepad1.bWasPressed());
+            telemetry.addData("Gamepad 1 B Released", gamepad1.bWasReleased());
+            telemetry.addData("Gamepad 1 B Status", gamepad1.b);
+
+            telemetry.addData("Gamepad 1 X Pressed", gamepad1.xWasPressed());
+            telemetry.addData("Gamepad 1 X Released", gamepad1.xWasReleased());
+            telemetry.addData("Gamepad 1 X Status", gamepad1.x);
+
+            telemetry.addData("Gamepad 1 Y Pressed", gamepad1.yWasPressed());
+            telemetry.addData("Gamepad 1 Y Released", gamepad1.yWasReleased());
+            telemetry.addData("Gamepad 1 Y Status", gamepad1.y);
+
+            // Add a note that the telemetry is only updated every 2 seconds
+            telemetry.addLine("\nTelemetry FPS = 20");
+
+            // Update the telemetry on the DS screen
+            
+            telemetry.update();
+            sleep(50);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestGmaepad.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class TestGmaepad extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("test gamepad1", "Press start to begin");
+        telemetry.update();
+        waitForStart();
+        while( opModeIsActive() ) {
+            telemetry.addData("Left Stick X", gamepad1.left_stick_x);
+            telemetry.addData("Left Stick Y", gamepad1.left_stick_y);
+            telemetry.addData("Right Stick X", gamepad1.right_stick_x);
+            telemetry.addData("Right Stick Y", gamepad1.right_stick_y);
+            telemetry.addData("Right trigger", gamepad1.right_trigger);
+            telemetry.addData("Left trigger", gamepad1.left_trigger);
+            telemetry.addData("Left Bumper", gamepad1.left_bumper);
+            telemetry.addData("Right Bumper", gamepad1.right_bumper);
+            telemetry.addData("A Button", gamepad1.a);
+            telemetry.addData("B Button", gamepad1.b);
+            telemetry.addData("X Button", gamepad1.x);
+            telemetry.addData("Y Button", gamepad1.y);
+            telemetry.addData("Left Stick Button", gamepad1.left_stick_button);
+            telemetry.addData("Right Stick Button", gamepad1.right_stick_button);
+            telemetry.addData("Dpad_down",gamepad1.dpad_down);
+            telemetry.addData("Dpad_down", gamepad1.dpad_up);
+            telemetry.addData("Dpad_left", gamepad1.dpad_left);
+            telemetry.addData("Dpad_right", gamepad1.dpad_right);
+            telemetry.addData("Start Button", gamepad1.start);
+            telemetry.addData("Back Button", gamepad1.back);
+
+            telemetry.update();
+        }
+    }
+}
