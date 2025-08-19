5de89a236cc3fb2ed04bbd5178c6ef3495affdcb
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2025-08-19 22:13:32 +0800

    Create SixServoArmCalculationTestor.java
    准备将来测试

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/sixservoarm/SixServoArmCalculationTestor.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.controllers.sixservoarm;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class SixServoArmCalculationTestor extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
