91ecc0ffc5e50ccaf953a9cd6891a016939c246a
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2025-08-20 08:30:06 +0800

    增加OpMode命名与组别

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/sixservoarm/SixServoArmCalculationTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/sixservoarm/SixServoArmCalculationTester.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode.controllers.sixservoarm;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp
-public class SixServoArmCalculationTestor extends LinearOpMode {
+@TeleOp(name = "SixServoArmCalculationTester", group = "Test")
+public class SixServoArmCalculationTester extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
