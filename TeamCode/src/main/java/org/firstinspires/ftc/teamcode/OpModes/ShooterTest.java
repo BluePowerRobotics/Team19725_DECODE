b362574eac29d3038d9e2a145172615df1683b63
Zhz2025 <Zhanghz270140@163.com>
2025-09-23 18:22:57 +0800

    增加计算射球位置--蓝色方还需debug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ShooterTest.java
@@ -1,20 +1,33 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.controllers.Shooter;
+import org.firstinspires.ftc.teamcode.utility.SolveShootPoint;
 
 
 @TeleOp
 @Config
 public class ShooterTest extends LinearOpMode {
+    public SolveShootPoint solveShootPoint = new SolveShootPoint();
+    public static double R = 48 * Math.sqrt(2);
+    public static double x = 24;
+    public static double y = 0;
     public Shooter shooter;
     public static double initSpeed = 300;
     @Override
     public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         shooter = new Shooter(hardwareMap, telemetry);
+        telemetry.addData("RED:ANSX, ANSY, ANSTheta", solveShootPoint.solveREDShootPoint(new Pose2d(x, y, 0), R));
+        telemetry.addData("BLUE:ANSX, ANSY, ANSTheta", solveShootPoint.solveBLUEShootPoint(new Pose2d(x, y, 0), R));
+
+        telemetry.update();
         waitForStart();
         double targetSpeed = 0;
         while (opModeIsActive()) {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/SolveShootPoint.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.utility;
+
+import com.acmerobotics.roadrunner.Pose2d;
+
+public class SolveShootPoint {
+    private final double genhao2 = Math.sqrt(2);
+    public Pose2d solveREDShootPoint(Pose2d robotPose, double R) {
+        double x0 = robotPose.position.x;
+        double y0 = robotPose.position.y;
+        double ansX = 0;
+        double ansY = 0;
+
+        //初始点在发射区内，找圆和车-球门连线的交点
+        if(x0 < y0){
+            double tmpx = 1 + Math.pow((72 - y0)/(72 - x0), 2);
+            ansX = 72 - Math.sqrt(R*R / tmpx);
+            ansY = 72 + (72 - y0)/(72 - x0) * (ansX - 72);
+        }
+        //初始点在发射区外，找圆和发射区边界的交点
+        else{
+            ansX = 72 - genhao2 / 2 * R;
+            ansY = 72 - genhao2 / 2 * R;
+        }
+        return new Pose2d(ansX, ansY, Math.atan((72 - ansY) / (72 - ansX)));
+    }
+    public Pose2d solveBLUEShootPoint(Pose2d robotPose, double R) {
+        double x0 = robotPose.position.x;
+        double y0 = robotPose.position.y;
+        double ansX = 0;
+        double ansY = 0;
+
+        //初始点在发射区内，找圆和车-球门连线的交点
+        if(-x0 < y0){
+            double tmpx = 1 + Math.pow((72 - y0)/(72 - x0), 2);
+            ansX = -72 + Math.sqrt(R*R / tmpx);
+            ansY = 72 + (72 - y0)/(-72 - x0) * (ansX +72);
+        }
+        //初始点在发射区外，找圆和发射区边界的交点
+        else{
+            ansX = -72 + genhao2 / 2 * R;
+            ansY = 72 - genhao2 / 2 * R;
+        }
+        return new Pose2d(ansX, ansY, (Math.atan(( 72 - ansY) / (-ansX - 72)) + Math.PI));
+    }
+}
