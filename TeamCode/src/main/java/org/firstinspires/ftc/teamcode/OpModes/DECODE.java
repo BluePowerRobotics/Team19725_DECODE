50b1ec23bbb96a635def7e87fc6644159674cd6e
Zhz2025 <Zhanghz270140@163.com>
2025-10-14 21:41:27 +0800

    在主程序中添加底盘

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/DECODE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/DECODE.java
@@ -3,11 +3,15 @@ package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Rotation2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.RoadRunner.Drawing;
 import org.firstinspires.ftc.teamcode.controllers.Sweeper;
 import org.firstinspires.ftc.teamcode.controllers.chassis.ChassisController;
 import org.firstinspires.ftc.teamcode.controllers.shooter.Shooter;
@@ -15,7 +19,8 @@ import org.firstinspires.ftc.teamcode.utility.Point2D;
 
 @TeleOp(name="DECODE", group="OpModes")
 public class DECODE extends LinearOpMode {
-    public ChassisController chassisController;
+    //todo add sweeper enum
+    public ChassisController chassis;
     public Sweeper sweeper;
     public Shooter shooter1;
     public Shooter shooter2;
@@ -30,9 +35,15 @@ public class DECODE extends LinearOpMode {
         sweeper = new Sweeper(hardwareMap);
         shooter1 = new Shooter(hardwareMap, telemetry, "shooterMotor1", true);
         shooter2 = new Shooter(hardwareMap, telemetry, "shooterMotor2", false);
-        chassisController = new ChassisController(hardwareMap, new Point2D(0,0), 0);
+        chassis = new ChassisController(hardwareMap, new Point2D(0,0), 0);
     }
     void Telemetry(){
+
+        telemetry.addData("NoHeadModeStartError:",chassis.noHeadModeStartError);
+        telemetry.addData("NoHeadMode",chassis.useNoHeadMode?"NoHead":"Manual");
+        telemetry.addData("RunMode",chassis.runningToPoint?"RUNNING_TO_POINT":"MANUAL");
+        telemetry.addData("Position(mm)",chassis.robotPosition.getData().getPosition(DistanceUnit.MM).toString());
+        telemetry.addData("Position(inch)",chassis.robotPosition.getData().getPosition(DistanceUnit.INCH).toString());
         telemetry.addData("targetSpeed", targetSpeed);
         telemetry.addData("1-power * 1000", shooter1.getPower() * 1000);
         telemetry.addData("1-speed", shooter1.getCurrent_speed());
@@ -62,6 +73,36 @@ public class DECODE extends LinearOpMode {
         }
 
     }
+    void chassis(){
+        double drive = -gamepad1.left_stick_y-gamepad2.left_stick_y; // 前后
+        double strafe = gamepad1.left_stick_x; // 左右
+        double rotate = gamepad1.right_stick_x; // 旋转
+        chassis.gamepadInput(strafe, drive, rotate);
+        if(gamepad1.xWasReleased()) chassis.exchangeNoHeadMode();
+        if(gamepad1.yWasReleased()) {
+            chassis.setTargetPoint(new Pose2d(new Vector2d(0,0), Rotation2d.fromDouble(0)));
+        }
+        telemetry.addData("y-power",drive);
+        telemetry.addData("x-power",strafe);
+        telemetry.addData("r-power",rotate);
+
+        Pose2d pose = chassis.robotPosition.mecanumDrive.localizer.getPose();
+
+        TelemetryPacket packet = new TelemetryPacket();
+        packet.fieldOverlay().setStroke("#3F51B5");
+        Drawing.drawRobot(packet.fieldOverlay(), pose);
+        FtcDashboard.getInstance().sendTelemetryPacket(packet);
+    }
+
+    void eat(){
+        if(gamepad1.left_bumper || gamepad2.left_bumper){
+            sweeper.Eat();
+        }
+        if(gamepad1.right_bumper || gamepad2.right_bumper){
+            sweeper.output();
+        }
+    }
+
 
 
     @Override
@@ -69,6 +110,8 @@ public class DECODE extends LinearOpMode {
         Init();
         waitForStart();
         while (opModeIsActive()) {
+            shoot();
+            chassis();
             Telemetry();
         }
     }
