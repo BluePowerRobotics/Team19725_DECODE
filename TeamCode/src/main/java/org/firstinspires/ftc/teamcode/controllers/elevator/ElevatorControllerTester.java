c5239a49613af6969df8e63d0c20e4ae97590b6f
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2025-10-23 20:13:17 +0800

    加入爬升控制测试器至TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/elevator/ElevatorControllerTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/elevator/ElevatorControllerTester.java
@@ -2,8 +2,10 @@ package org.firstinspires.ftc.teamcode.controllers.elevator;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @Config
+@TeleOp(name = "ElevatorControllerTester", group = "TEST")
 public class ElevatorControllerTester extends LinearOpMode {
     public static double BalancePower=0;
     boolean directControl=false;
