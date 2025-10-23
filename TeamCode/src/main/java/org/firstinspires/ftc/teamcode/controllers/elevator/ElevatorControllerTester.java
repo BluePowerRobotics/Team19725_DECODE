d671d62c6df8484209ff4fe46f8c93d469bf6c0a
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2025-10-23 20:11:17 +0800

    爬升控制器测试器及样例代码

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/elevator/ElevatorControllerTester.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.controllers.elevator;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Config
+public class ElevatorControllerTester extends LinearOpMode {
+    public static double BalancePower=0;
+    boolean directControl=false;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ElevatorController elevatorController = new ElevatorController(hardwareMap);
+        waitForStart();
+        while (opModeIsActive()) {
+            if(gamepad1.right_stick_y!=0) {
+                directControl=true;
+                elevatorController.setPower(BalancePower-gamepad1.right_stick_y);
+            }else{
+                if(directControl){
+                    directControl=false;
+                    elevatorController.setPosition(elevatorController.elevatorMotor.getCurrentPosition());
+                }
+            }
+            telemetry.addData("gamePad1:RY",gamepad1.right_stick_y);
+            telemetry.addData("directControl",directControl);
+            telemetry.addData("BalancePower",BalancePower);
+            telemetry.update();
+        }
+    }
+}
