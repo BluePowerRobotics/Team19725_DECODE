877de1587ff7bd3eabb3fe7e1576d1f33052a44c
Zhz2025 <Zhanghz270140@163.com>
2025-10-24 20:47:10 +0800

    修正ManualFeedbackTuner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/tuning/ManualFeedbackTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/tuning/ManualFeedbackTuner.java
@@ -22,7 +22,7 @@ public final class ManualFeedbackTuner extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-            
+
             if (drive.localizer instanceof TwoDeadWheelLocalizer) {
                 if (TwoDeadWheelLocalizer.PARAMS.perpXTicks == 0 && TwoDeadWheelLocalizer.PARAMS.parYTicks == 0) {
                     throw new RuntimeException("Odometry wheel locations not set! Run AngularRampLogger to tune them.");
@@ -35,37 +35,13 @@ public final class ManualFeedbackTuner extends LinearOpMode {
             waitForStart();
 
             while (opModeIsActive()) {
-                if(gamepad1.xWasReleased()){
-                    mannual=!mannual;
-                }
-                if(mannual == true) {
-                    drive.setDrivePowers(new PoseVelocity2d(
-                            new Vector2d(
-                                    -gamepad1.left_stick_y,
-                                    0.0
-                            ),
-                            -gamepad1.right_stick_x
-                    ));
 
-                    drive.updatePoseEstimate();
-
-                    Pose2d pose = drive.localizer.getPose();
-                    telemetry.addData("x", pose.position.x);
-                    telemetry.addData("y", pose.position.y);
-                    telemetry.addData("heading (deg)", Math.toDegrees(pose.heading.toDouble()));
-                    telemetry.update();
-
-                    TelemetryPacket packet = new TelemetryPacket();
-                    packet.fieldOverlay().setStroke("#3F51B5");
-                    Drawing.drawRobot(packet.fieldOverlay(), pose);
-                    FtcDashboard.getInstance().sendTelemetryPacket(packet);
-                }else{
                     Actions.runBlocking(
                             drive.actionBuilder(new Pose2d(0, 0, 0))
                                     .lineToX(DISTANCE)
                                     .lineToX(0)
                                     .build());
-                }
+
             }
         } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
             TankDrive drive = new TankDrive(hardwareMap, new Pose2d(0, 0, 0));
