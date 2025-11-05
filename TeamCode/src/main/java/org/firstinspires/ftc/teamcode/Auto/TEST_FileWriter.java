6b6238c6c70b4df65a7c8e41bccbfe7b982ae888
Zhz2025 <Zhanghz270140@163.com>
2025-11-05 18:18:54 +0800

    删除无用文件

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TEST_FileWriter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TEST_FileWriter.java
@@ -10,8 +10,8 @@ public class TEST_FileWriter extends LinearOpMode {
         try (java.io.FileWriter writer = new java.io.FileWriter("/sdcard/FIRST/pose.txt")) {
             // 这里假设 drive 和 localizer 已经初始化，并且有 getPose() 方法
             // 示例数据，实际应替换为你的 drive.localizer.getPose()
-            double x = 1.23;
-            double y = 4.56;
+            double x = 11.23;
+            double y = 24.56;
             double heading = 0.78;
             writer.write(x + "," + y + "," + heading);
         } catch (java.io.IOException e) {
