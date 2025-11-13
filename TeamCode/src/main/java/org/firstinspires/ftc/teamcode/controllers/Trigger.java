9e8751c0ec674cfcb8b8c34621781fefcdccb7c0
Zhz2025 <Zhanghz270140@163.com>
2025-11-13 11:43:14 +0800

    增加trigger参数的config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/Trigger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/Trigger.java
@@ -10,8 +10,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Trigger {
     Servo triggerServo_LEFT;
     Servo triggerServo_RIGHT;
-    public double TRIGGER_OPEN_POS = 0;
-    public double TRIGGER_CLOSE_POS = 0.5;
+    public static double TRIGGER_OPEN_POS = 0;
+    public static double TRIGGER_CLOSE_POS = 0.4;
     public Trigger(HardwareMap hardwareMap){
         this.triggerServo_LEFT = hardwareMap.get(Servo.class, "trigger_left");
         this.triggerServo_RIGHT = hardwareMap.get(Servo.class, "trigger_right");
