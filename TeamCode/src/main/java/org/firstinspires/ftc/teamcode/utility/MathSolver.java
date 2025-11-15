aa5fe161d796390d9c1139cb54a22d14b6760c00
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2025-11-15 09:05:34 +0800

    恢复2维hypot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/MathSolver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/MathSolver.java
@@ -236,6 +236,19 @@ y_2
         return Inch*25.4;
     }
 
+    public static double hypot(double a, double b) {
+        double r;
+        if (Math.abs(a) > Math.abs(b)) {
+            r = b/a;
+            r = Math.abs(a)*Math.sqrt(1+r*r);
+        } else if (b != 0) {
+            r = a/b;
+            r = Math.abs(b)*Math.sqrt(1+r*r);
+        } else {
+            r = 0.0;
+        }
+        return r;
+    }
     public static double hypot(Number... numbers){
         double opr=0;
         for (Number number : numbers) {
