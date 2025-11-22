1e957041dae1c75f2eb070d15168d3e0b2a61ce4
Zhz2025 <Zhanghz270140@163.com>
2025-11-22 13:22:57 +0800

    测试kalmanFusionLocalizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/tuning/LocalizationTest_KalmanFusion.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/tuning/LocalizationTest_KalmanFusion.java
@@ -11,14 +11,15 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.RoadRunner.Drawing;
 import org.firstinspires.ftc.teamcode.RoadRunner.KalmanFusionLocalizer;
 import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive_Kalman;
 
 public class LocalizationTest_KalmanFusion extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-
-            MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-            drive.localizer=new KalmanFusionLocalizer(hardwareMap,MecanumDrive.PARAMS.inPerTick, new Pose2d(0, 0, 0));
+        final Pose2d START_POSE = new Pose2d(-64.8, 17.6, 0);
+            MecanumDrive_Kalman drive_kalman = new MecanumDrive_Kalman(hardwareMap, START_POSE);
+            MecanumDrive drive = new MecanumDrive(hardwareMap, START_POSE);
             waitForStart();
 
             while (opModeIsActive()) {
@@ -30,17 +31,26 @@ public class LocalizationTest_KalmanFusion extends LinearOpMode {
                         -gamepad1.right_stick_x
                 ));
 
+                drive_kalman.updatePoseEstimate();
                 drive.updatePoseEstimate();
 
-                Pose2d pose = drive.localizer.getPose();
+                Pose2d pose_kalman = drive_kalman.localizer.getPose();
+                telemetry.addData("kalmanx", pose_kalman.position.x);
+                telemetry.addData("kalmany", pose_kalman.position.y);
+                telemetry.addData("kalmanheading (deg)", Math.toDegrees(pose_kalman.heading.toDouble()));
+
+
+                Pose2d pose = drive_kalman.localizer.getPose();
                 telemetry.addData("x", pose.position.x);
                 telemetry.addData("y", pose.position.y);
-                telemetry.addData("heading (deg)", Math.toDegrees(pose.heading.toDouble()));
+                telemetry.addData("heading (deg)", Math.toDegrees(pose_kalman.heading.toDouble()));
                 telemetry.update();
 
                 TelemetryPacket packet = new TelemetryPacket();
                 packet.fieldOverlay().setStroke("#3F51B5");
                 Drawing.drawRobot(packet.fieldOverlay(), pose);
+                packet.fieldOverlay().setStroke("#4E9999");
+                Drawing.drawRobot(packet.fieldOverlay(), pose_kalman);
                 FtcDashboard.getInstance().sendTelemetryPacket(packet);
             }
     }
