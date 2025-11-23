1b9aeab2631d80524ba52aefa92e81c1e2e1d0ab
Zhz2025 <Zhanghz270140@163.com>
2025-11-23 22:40:01 +0800

    现在FileWriter可以自行设置位置

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TEST_FileWriter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TEST_FileWriter.java
@@ -1,18 +1,19 @@
 package org.firstinspires.ftc.teamcode.Auto;
-
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous
+@Config
 public class TEST_FileWriter extends LinearOpMode {
+    public static double x = -64.8;
+    public static double y = -17.6;
+    public static double heading = 0;
     @Override
     public void runOpMode() {
         try (java.io.FileWriter writer = new java.io.FileWriter("/sdcard/FIRST/pose.txt")) {
             // 这里假设 drive 和 localizer 已经初始化，并且有 getPose() 方法
             // 示例数据，实际应替换为你的 drive.localizer.getPose()
-            double x = 58.61417;
-            double y = 24;
-            double heading =0;
             writer.write(x + "," + y + "," + heading);
         } catch (java.io.IOException e) {
             throw new RuntimeException(e);
