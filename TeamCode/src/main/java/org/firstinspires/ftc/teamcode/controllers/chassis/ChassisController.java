6953312d41a753c44ae411c4f23b753873a552c9
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2025-11-30 01:58:35 +0800

    提升锁角表现：每次自动开始前重设PID 修正包格式 允许热调参

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/chassis/ChassisController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/chassis/ChassisController.java
@@ -3,11 +3,14 @@ package org.firstinspires.ftc.teamcode.controllers.chassis;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.controllers.chassis.locate.RobotPosition;
 import org.firstinspires.ftc.teamcode.utility.ActionRunner;
 import org.firstinspires.ftc.teamcode.utility.MathSolver;
+import org.firstinspires.ftc.teamcode.utility.PIDController;
 import org.firstinspires.ftc.teamcode.utility.Point2D;
 
 @Config
@@ -32,7 +35,7 @@ public class ChassisController {
     double HeadingLockRadian;
     public double noHeadModeStartError;
     public ActionRunner actionRunner=new ActionRunner();
-    public static org.firstinspires.ftc.teamcode.controllers.chassis.ChassisCalculator chassisCalculator= new ChassisCalculator();
+    public static ChassisCalculator chassisCalculator= new ChassisCalculator();
     ChassisOutputter chassisOutputter;
 
     /**
@@ -124,6 +127,7 @@ public class ChassisController {
                     }else{
                         if(HeadingLockRadianReset){
                             HeadingLockRadianReset=false;
+                            chassisCalculator.pidRadian.reset();
                             HeadingLockRadian=robotPosition.getData().headingRadian;
                         }
                         omega=chassisCalculator.calculatePIDRadian(HeadingLockRadian,robotPosition.getData().headingRadian);
@@ -138,4 +142,161 @@ public class ChassisController {
         }
     }
 }
+@Config
+class ChassisCalculator {
+    public static class Params {
+        //todo 调整参数
+        public double rb = 0.23; // rb 车轮中心到机器人中心的基本半径 (m)
+        // rb 车轮中心到机器人中心的基本半径 (m)
+        public double pkP = 0.002;//point k
+        public double pkI = 0;
+        public double pkD = 0.00025;
+        public double rkP = 0.7;//radian k
+        public double rkI = 0;
+        public double rkD = 0.1;
+    }
+    public static Params PARAMS = new Params();
 
+    PIDController pidPoint;
+    PIDController pidRadian;
+
+    ChassisCalculator() {
+        // 私有构造函数，防止外部实例化
+        pidPoint = new PIDController(PARAMS.pkP, PARAMS.pkI, PARAMS.pkD);
+        pidRadian = new PIDController(PARAMS.rkP, PARAMS.rkI, PARAMS.rkD);
+    }
+
+    /**
+     * 逆运动学公式
+     *
+     * @param vx    机器人相对于自身的横移速度 (m/s) —— +右
+     * @param vy    机器人相对于自身的前进速度 (m/s) —— +前
+     * @param omega 机器人旋转角速度 (rad/s) —— +逆时针
+     * @return 各个车轮的线速度 (m/s)
+     */
+    public double[] solveChassis(double vx, double vy, double omega) {
+        double v_fl = vy + vx - (2 * PARAMS.rb * omega);
+        double v_bl = vy - vx - (2 * PARAMS.rb * omega);
+        double v_br = vy + vx + (2 * PARAMS.rb * omega);
+        double v_fr = vy - vx + (2 * PARAMS.rb * omega);
+
+        return new double[]{v_fl, v_fr, v_bl, v_br};
+    }
+
+    /**
+     * 逆运动学公式（地面坐标系）
+     * @param vx 机器人相对于地面的横移速度 (m/s) —— +右
+     * @param vy 机器人相对于地面的前进速度 (m/s) —— +前
+     * @param omega 机器人旋转角速度 (rad/s) —— +逆时针
+     * @param headingRadian 机器人朝向，弧度制
+     * @return 各个车轮的线速度 (m/s)
+     */
+    public double[] solveGround(double vx, double vy, double omega, double headingRadian) {
+
+        double vxPro = vx * Math.cos(headingRadian) + vy * Math.sin(headingRadian);
+        double vyPro = -vx * Math.sin(headingRadian) + vy * Math.cos(headingRadian);
+        return solveChassis(vxPro, vyPro, omega);
+    }
+    public double[] solveGround(double[] vxy,double vOmega,double headingRadian){
+        return solveGround(vxy[0],vxy[1],vOmega,headingRadian);
+    }
+
+    long lastTimeXY = 0;
+    boolean firstRunXY = true;
+
+    public double[] calculatePIDXY(Point2D target, Point2D current) {
+        if (firstRunXY) {
+            firstRunXY = false;
+            lastTimeXY = System.nanoTime();
+            pidPoint.reset();
+        }
+        double errorX = target.getX() - current.getX();
+        double errorY = target.getY() - current.getY();
+        double distance = Math.hypot(errorX, errorY);
+        double angleToTarget = Math.atan2(errorY, errorX);
+        pidPoint.setPID(PARAMS.pkP, PARAMS.pkI, PARAMS.pkD);
+        double output = pidPoint.calculate(distance, 0, (System.nanoTime() - lastTimeXY) / 1e9);
+        lastTimeXY = System.nanoTime();
+        double vx = output * Math.cos(angleToTarget);
+        double vy = output * Math.sin(angleToTarget);
+        return new double[]{vx, vy};
+    }
+
+    long lastTimeRadian = 0;
+    boolean firstRunRadian = true;
+
+    public double calculatePIDRadian(double targetRadian, double currentRadian) {
+        if (firstRunRadian) {
+            firstRunRadian = false;
+            lastTimeRadian = System.nanoTime();
+            pidRadian.reset();
+        }
+        double errorRadian = targetRadian - currentRadian;
+        // 归一化到[-π, π]
+        errorRadian = MathSolver.normalizeAngle(errorRadian);
+        pidRadian.setPID(PARAMS.rkP, PARAMS.rkI, PARAMS.rkD);
+        double output = pidRadian.calculate(errorRadian, 0, (System.nanoTime() - lastTimeRadian) / 1e9);
+        lastTimeRadian = System.nanoTime();
+        return output;
+    }
+}
+class ChassisOutputter {
+    public static class Params {
+        //todo 调整参数
+        double CPR = ((((1.0 + (46.0 / 17.0))) * (1.0 + (46.0 / 11.0))) * 28.0); // 电机每转一圈的编码器脉冲数
+        double wheelDiameter = 104; // 轮子直径 (mm)
+        double mmPerTick = (wheelDiameter * Math.PI) / CPR; // 每个编码器脉冲对应的线性位移 (mm)
+        double maxRpm = 312; // 电机最大转速 (RPM)
+    }
+    public static Params PARAMS = new Params();
+
+    HardwareMap hardwareMap;
+    DcMotorEx leftFront, rightFront, leftBack, rightBack;
+
+    ChassisOutputter(HardwareMap hardwareMap) {
+        //TODO 以下配置需要与MecanumDrive.java中保持一致
+        this.hardwareMap = hardwareMap;
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    /**
+     * 设置各个车轮的线速度 (m/s)
+     *
+     * @param v_fl 左前轮速度
+     * @param v_fr 右前轮速度
+     * @param v_bl 左后轮速度
+     * @param v_br 右后轮速度
+     */
+    public void setWheelVelocities(double v_fl, double v_fr, double v_bl, double v_br) {
+        v_fl = v_fl * 1000 / PARAMS.wheelDiameter;// (m/s) -> (r/s)
+        v_fr = v_fr * 1000 / PARAMS.wheelDiameter;
+        v_bl = v_bl * 1000 / PARAMS.wheelDiameter;
+        v_br = v_br * 1000 / PARAMS.wheelDiameter;
+        if(Math.abs(v_fl) > PARAMS.maxRpm / 60 || Math.abs(v_fr) > PARAMS.maxRpm / 60 || Math.abs(v_bl) > PARAMS.maxRpm / 60 || Math.abs(v_br) > PARAMS.maxRpm / 60){
+            double maxV = Math.max(Math.max(Math.abs(v_fl), Math.abs(v_fr)), Math.max(Math.abs(v_bl), Math.abs(v_br)));
+            v_fl = v_fl / maxV * PARAMS.maxRpm / 60;// range to [-maxRpm/60, maxRpm/60]
+            v_fr = v_fr / maxV * PARAMS.maxRpm / 60;
+            v_bl = v_bl / maxV * PARAMS.maxRpm / 60;
+            v_br = v_br / maxV * PARAMS.maxRpm / 60;
+        }
+        leftFront.setPower(v_fl / (PARAMS.maxRpm / 60));
+        rightFront.setPower(v_fr / (PARAMS.maxRpm / 60));
+        leftBack.setPower(v_bl / (PARAMS.maxRpm / 60));
+        rightBack.setPower(v_br / (PARAMS.maxRpm / 60));
+    }
+
+    public void setWheelVelocities(double[] velocities) {
+        setWheelVelocities(velocities[0], velocities[1], velocities[2], velocities[3]);
+    }
+}
